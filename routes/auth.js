6ebb322f02a32b58f1042f59446a2a94f5b3a0d8
Abhishek Manjhi <90549183+Abhishekknown@users.noreply.github.com>
2024-03-26 01:10:44 +0530

    return gone

--- routes/auth.js
+++ routes/auth.js
@@ -81,7 +81,7 @@ router.post("/signup", redirectAuth,
             });
         } catch (error) {
             console.log(error)
-            return res.status(500).json({ msg: error })
+            res.status(500).json({ msg: error })
         }
     }
 )
